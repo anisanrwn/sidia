5396cad7734090c562c9ac5e6108b744e2ac9e01
anisanrwn <nirwanaanisa1508@gmail.com>
2025-02-25 13:13:56 +0700

    Update AdminController.php

---
+++ app/Controllers/AdminController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers;
+
+class AdminController extends BaseController
+{
+    public function sidebaradmin()
+    {
+        return view('templates/sidebaradmin'); 
+
+    }
+}
